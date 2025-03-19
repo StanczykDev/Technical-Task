2a9ac5fad65751e4a2db8695004e9f2918e75dd5
StanczykDev <stanczyk.dev@protonmail.com>
2025-03-19 20:08:33 +0100

    Markup complete.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { ReactComponent as LogoIcon } from "./assets/LogoIcon.svg";
 import { ReactComponent as LogoTitle } from "./assets/LogoTitle.svg";
 import { ReactComponent as MenuIcon } from "./assets/MenuIcon.svg";
+import { ReactComponent as CloseIcon } from "./assets/CloseIcon.svg";
 import './App.sass';
+import { useCallback, useState } from "react";
+
+const menuItemContents = ['Home', 'Products', <span className="wheelCartIcon"></span>];
 
 const cardsTitles = ['Move the borders of reality!', 'Space is not just stars and planets', 'For those who dream of stars', 'Fulfill your fantastic dreams'];
 const cardsSubTitles = ['Go on a space adventure', 'it is a majestic journey to ', 'Our offer: make your dream come true', 'Space has never been so close'];
@@ -24,7 +28,19 @@ function Card({ title, subTitle }) {
         </div>
 }
 
+function MenuItem({ children }) {
+  return <li className="menuItem">
+      <button className="menuItemButton">{children}</button>
+    </li>
+}
+
 function App() {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  const onMenuClick = useCallback(() => {
+    setIsMenuOpened(!isMenuOpened);
+  }, [isMenuOpened]);
+
   return (
     <div className="App">
       <header className="header">
@@ -32,9 +48,15 @@ function App() {
           <div className="icon"><LogoIcon /></div>
           <div className="title"><LogoTitle /></div>
         </div>
-        <button className="menu">
-          <div className="icon"><MenuIcon /></div>
+        <button className="menuButton" onClick={onMenuClick}>
+          {!isMenuOpened 
+            ? <div className="iconMenu"><MenuIcon /></div>
+            : <div className="iconClose"><CloseIcon /></div>
+          }
         </button>
+        <ul className={`menuList${isMenuOpened ? ' opened' : ''}`} style={{ 'listStyleType': 'none '}}>
+          {menuItemContents.map(content => <MenuItem key={content.toString()}>{content}</MenuItem>)}
+        </ul>
       </header>
       <main className="main">
         <div className="banner">
@@ -48,14 +70,21 @@ function App() {
         </div>
         <div className="offers">
           <h3 className="offersSubTitle">Offers</h3>
-          {cardsTitles.map((title, index) => <Card title={title} subTitle={cardsSubTitles[index]} />)}
+          <div className="offersCards">
+            {cardsTitles.map((title, index) => <Card title={title} subTitle={cardsSubTitles[index]} key={title} />)}
+          </div>
         </div>
         <div className="description">
-          <h3 className="de1531547scriptionSubTitle">Embark on a space journey</h3>
+          <h3 className="descriptionSubTitle">Embark on a space journey</h3>
           <p className="descriptionText">
             Traveling into space is one of the most exciting and unforgettable adventures that can change your life forever. And if you have ever dreamed of exploring stars, planets and galaxies, then our company is ready to help you realize this dream. We offer a unique experience that will allow you to go on a space journey and see all the secrets of the universe. We guarantee that every moment in space will be filled with incredible impressions, excitement and new discoveries. Our team of professionals takes care of your safety and comfort so that you can fully enjoy your adventure in space. We offer various options for space excursions.
           </p>
-          <button className="descriptionInfoButton">Read more</button>
+          <p className="descriptionText hiddenContent">
+            Traveling into space is one of the most exciting and unforgettable adventures that can change your life forever. And if you have ever dreamed of exploring stars, planets and galaxies, then our company is ready to help you realize this dream. We offer a unique experience that will allow you to go on a space journey and see all the secrets of the universe. We guarantee that every moment in space will be filled with incredible impressions, excitement and new discoveries. Our team of professionals takes care of your safety and comfort so that you can fully enjoy your adventure in space. We offer various options for space excursions.
+          </p>
+          <div className="descriptionInfoButtonContainer">
+            <button className="descriptionInfoButton">Read more</button>
+          </div>
         </div>
       </main>
       <footer className="footer">
